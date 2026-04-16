609ba42bf2b15ef8591d1decf5750ed9d1bbb187
Source-C0de <mfahim.shahriar50@gmail.com>
2026-04-16 15:05:54 +0300

    Auto-commit: updated server_stats.sh  at  +'%Y-%m-%d %H-%M-%S')

--- server_stats.sh
+++ server_stats.sh
@@ -3,7 +3,14 @@
 
 echo "=== Server Performance report ==="
 
-
+# Cpu used 
 CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/')
 CPU_USED=$(awk "BEGIN {printf \"%.1f%%\", 100-$CPU_IDLE }")
 echo "🔹 CPU Usage: $CPU_USED"
+
+
+# Memory Used
+MEM_USED_PCT=$(free -m | awk '/^Mem:/ {printf "%.1f", ($3/$2)*100}')
+MEM_FREE_PCT=$(free -m | awk '/^Mem:/ {printf "%.1f", ($4/$2)*100}')
+echo " Memory: Used ${MEM_USED_PCT}% | Free ${MEM_FREE_PCT}%"
+
